c4f2ee60e9d5e593a65d4d3f9127132273da1c08
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2022-05-11 20:25:08 +0600

    updated property list

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,18 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
     selector: 'app-property-card',
-    // template: `<h1>I am a card</h1>`,
     templateUrl: 'property-card.component.html',
-    // styles:['h1{font-weight: normal}']
     styleUrls: ['property-card.component.css']
 })
 
 export class PropertyCardComponent {
-    Property: any = {
-        "Id": 1,
-        "Name": "Birla House",
-        "Type": "House",
-        "Price": 12000
-    };
+    @Input() property: any;
 }
--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-property-list',
-  templateUrl: './property-list.component.html',
-  styleUrls: ['./property-list.component.css']
+    selector: 'app-property-list',
+    templateUrl: './property-list.component.html',
+    styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
 
-  constructor() { }
+    properties: Array<any> = [
+        {
+            "Id": 1,
+            "Name": "Birla House",
+            "Type": "House",
+            "Price": 12000
+        },
+        {
+            "Id": 2,
+            "Name": "Err Villa",
+            "Type": "Apartment",
+            "Price": 5000
+        },
+        {
+            "Id": 3,
+            "Name": "Home de Viru",
+            "Type": "House",
+            "Price": 16000
+        },
+        {
+            "Id": 4,
+            "Name": "Fashion House",
+            "Type": "Apartment",
+            "Price": 2000
+        },
+        {
+            "Id": 5,
+            "Name": "Test House",
+            "Type": "House",
+            "Price": 10000
+        },
+        {
+            "Id": 6,
+            "Name": "Nice Apartment",
+            "Type": "Apartment",
+            "Price": 7000
+        }
+    ];
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
 
 }

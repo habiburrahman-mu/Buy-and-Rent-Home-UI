b2dacdd48d1edd3b5af71be7bb17e306ce81f7e3
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2022-05-22 18:17:57 +0600

    added feature: buy-rent wise product showing

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HousingService} from "../../services/housing.service";
 import {IProperty} from "../IProperty.interface";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
     selector: 'app-property-list',
@@ -8,15 +9,19 @@ import {IProperty} from "../IProperty.interface";
     styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
-
+    SellRent: number = 1;
     properties: Array<IProperty> = [];
 
-    constructor(private housingService: HousingService) {
+    constructor(private housingService: HousingService, private route: ActivatedRoute) {
     }
 
     ngOnInit(): void {
-        this.housingService.getAllProperties().subscribe(data => {
+        if (this.route.snapshot.url.toString()) {
+            this.SellRent = 2;
+        }
+        this.housingService.getAllProperties(this.SellRent).subscribe(data => {
             this.properties = data;
+            console.log(this.route.snapshot.url.toString());
         }, error => {
             console.log(error);
         });
--- src/app/services/housing.service.ts
+++ src/app/services/housing.service.ts
@@ -12,12 +12,13 @@ export class HousingService {
     constructor(private http: HttpClient) {
     }
 
-    getAllProperties(): Observable<IProperty[]> {
+    getAllProperties(SellRent: number): Observable<IProperty[]> {
         return this.http.get('data/properties.json').pipe(
             map(data => {
                const propertiesArray: Array<IProperty> = [];
                for(const id in data) {
-                   if(data.hasOwnProperty(id)) {
+                   // @ts-ignore
+                   if(data.hasOwnProperty(id) && data[id].SellRent === SellRent) {
                        // @ts-ignore
                        propertiesArray.push(data[id]);
                    }

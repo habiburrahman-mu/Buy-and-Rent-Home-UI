5f03cbaf5db7079adc24cc934559b2188e577a74
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2022-06-14 23:09:02 +0600

    added pipe in property list component

--- src/app/Pipes/sort.pipe.ts
+++ src/app/Pipes/sort.pipe.ts
@@ -1,22 +1,29 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
-  name: 'sort'
+    name: 'sort'
 })
 export class SortPipe implements PipeTransform {
 
-  transform(initialArr: Array<string>, args: any[]): any {
-      const sortField = args[0];
-      initialArr.sort((a: any, b: any) => {
-         if(a[sortField] < b[sortField]) {
-             return -1;
-         } else if (a[sortField] > b[sortField]) {
-             return 1;
-         } else {
-             return 0;
-         }
-      });
-    return initialArr;
-  }
+    transform(initialArr: Array<string>, args: any[]): any {
+        const sortField = args[0];
+        const sortDirection = args[1];
+        let multiplier = 1;
+
+        if (sortDirection == 'desc') {
+            multiplier = -1;
+        }
+
+        initialArr.sort((a: any, b: any) => {
+            if (a[sortField] < b[sortField]) {
+                return -1 * multiplier;
+            } else if (a[sortField] > b[sortField]) {
+                return 1 * multiplier;
+            } else {
+                return 0;
+            }
+        });
+        return initialArr;
+    }
 
 }

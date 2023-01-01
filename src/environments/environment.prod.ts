a3cedb6c8dc982e51fb6acf5ce06b69204c2a2e4
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2023-01-01 10:33:07 +0600

    Updated environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  production: true,
-    baseUrl: "http://apis.mh-rahman.com:1001"
+    production: true,
+    baseUrl: "http://apis.mh-rahman.com:1001",
+    staticFilePath: "/staticfiles"
 };

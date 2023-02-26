5d81c0c55f2f9c4a06b314406cdbe5999047346b
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2023-02-26 23:13:16 +0600

    Updated UI of property detail

--- src/app/modules/property-public/property-detail/property-detail.component.ts
+++ src/app/modules/property-public/property-detail/property-detail.component.ts
@@ -1,18 +1,21 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PhotoDto } from 'src/app/models/photoDto';
 import { PropertyDetailDto } from 'src/app/models/propertyDetailDto';
 import { PhotoService } from 'src/app/services/photo.service';
 import { PropertyService } from 'src/app/services/property.service';
 import { environment } from 'src/environments/environment';
 
+import * as L from 'leaflet';
+
 @Component({
     selector: 'app-property-detail',
     templateUrl: './property-detail.component.html',
     styleUrls: ['./property-detail.component.css']
 })
-export class PropertyDetailComponent {
+export class PropertyDetailComponent implements OnInit, AfterViewInit {
+    private map: any;
     public propertyId: number = 0;
     staticFileUrl: string = environment.baseUrl + environment.staticFilePath;
     propertyData: PropertyDetailDto;
@@ -20,16 +23,16 @@ export class PropertyDetailComponent {
     photos: PhotoDto[] = [];
 
     constructor(private route: ActivatedRoute,
-                private router: Router,
-                private propertyService: PropertyService,
-                private photoService: PhotoService) {
+        private router: Router,
+        private propertyService: PropertyService,
+        private photoService: PhotoService) {
     }
 
     //
     ngOnInit() {
         this.route.params.subscribe(params => {
             this.propertyId = params['id'];
-            if(this.propertyId > 0) {
+            if (this.propertyId > 0) {
                 this.loadPropertyImages();
             }
         });
@@ -38,6 +41,37 @@ export class PropertyDetailComponent {
         });
 
 
+    }
+
+    ngAfterViewInit(): void {
+        this.initMap();
+    }
+
+    private initMap(): void {
+        // this.map = L.map('map', {
+        //   center: [ 39.8282, -98.5795 ],
+        //   zoom: 3
+        // });
+
+        // const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        //   maxZoom: 18,
+        //   minZoom: 3,
+        //   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        // });
+
+        // tiles.addTo(this.map);
+        // 23.780279, 90.416765
+        var map = L.map('map').setView([23.780279, 90.416765], 12);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([23.780279, 90.416765]).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
+
+        this.map = map;
     }
 
     loadPropertyImages() {

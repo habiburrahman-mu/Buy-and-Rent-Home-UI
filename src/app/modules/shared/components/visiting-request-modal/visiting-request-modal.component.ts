069e07f80c94e50e58e87afa1ed2a6d85627f406
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2023-09-11 23:20:34 +0600

    Updated design for visiting request modal

--- src/app/modules/shared/components/visiting-request-modal/visiting-request-modal.component.ts
+++ src/app/modules/shared/components/visiting-request-modal/visiting-request-modal.component.ts
@@ -9,9 +9,19 @@ import { PropertyService } from 'src/app/services/http/property.service';
 export class VisitingRequestModalComponent implements OnInit {
 
 	@Input() isVisitingRequestModalVisible = false;
+	@Input() propertyId: number;
 	@Output() isVisitingRequestModalVisibleChange: EventEmitter<boolean> = new EventEmitter<boolean>();
 
 	selectedDateIndex: any = null;
+
+	date: Date | undefined;
+
+	todayDate = new Date();
+
+	minDate = new Date();
+	maxDate = new Date();
+
+	disabledDates: Date[] = [];
 
 	timeSlots: any[] = [];
 
@@ -44,22 +54,50 @@ export class VisitingRequestModalComponent implements OnInit {
 
 	constructor(
 		private propertyService: PropertyService
-	) { }
+	) {
+		this.minDate.setDate(this.todayDate.getDate() + 1);
+		this.maxDate.setDate(this.todayDate.getDate() + 7);
+	}
 
 	ngOnInit(): void {
 		console.log(this.products);
-		this.propertyService.getAvailableSlotsForNext10Days(1).subscribe({
+		this.propertyService.getAvailableSlotsForNext10Days(this.propertyId).subscribe({
 			next: (response) => {
 				this.products = response.map((x: any, index: number) => {
 					return {
 						index: index,
-						date: new Date(x.date),
+						date: new Date(new Date(x.date).toDateString()),
 						name: x.day,
 						timeSlots: x.availableHours
 					}
 				});
+
+				this.disableOwnerNonAvailableDates();
+
+				console.log(this.products);
 			}
 		});
+
+		// console.log(this.todayDate, this.minDate, this.maxDate);
+	}
+
+	private disableOwnerNonAvailableDates() {
+		let disabledDates: Date[] = [];
+
+		const startDate = new Date(this.minDate);
+		const endDate = new Date(this.maxDate);
+
+		let iterator = new Date(startDate);
+
+		while (iterator <= endDate) {
+			let dateExist = this.products.some(x => x.date.toDateString() === iterator.toDateString());
+			if (!dateExist) {
+				disabledDates.push(new Date(iterator));
+			}
+			iterator.setDate(iterator.getDate() + 1);
+		}
+
+		this.disabledDates = disabledDates;
 	}
 
 	onHideVisitingRequestModalModal() {
@@ -70,5 +108,14 @@ export class VisitingRequestModalComponent implements OnInit {
 	onClickDateSlot(selectedDay: any) {
 		this.selectedDateIndex = selectedDay.index;
 		this.timeSlots = selectedDay.timeSlots;
+	}
+
+	onSelectDate(selectedDate: Date) {
+		console.log(selectedDate, selectedDate.toDateString());
+		let prod = this.products.find(x => x.date.toDateString() === selectedDate.toDateString());
+		if (prod) {
+			this.selectedDateIndex = prod.index;
+			this.timeSlots = prod.timeSlots;
+		}
 	}
 }

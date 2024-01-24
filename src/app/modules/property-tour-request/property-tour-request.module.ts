397bd57dbbd983e025e878502cc871d47412a8ca
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2024-01-24 22:48:43 +0600

    :truck: renamed: renamed pending tour request fies to tour request

--- src/app/modules/property-tour-request/property-tour-request.module.ts
+++ src/app/modules/property-tour-request/property-tour-request.module.ts
@@ -5,23 +5,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { PendingTourRequestComponent } from './pending-tour-request/pending-tour-request.component';
 import { TourRequestByPropertyComponent } from './tour-request-by-property/tour-request-by-property.component';
-import { PendingTourRequestCardComponent } from './pending-tour-request-card/pending-tour-request-card.component';
+import { PendingTourRequestCardComponent } from './tour-request-card/tour-request-card.component';
 
 const routes: Routes = [
-	{path: "", component: PropertyTourRequestShellComponent},
+	{ path: "", component: PropertyTourRequestShellComponent },
 ];
 
 @NgModule({
-  declarations: [
+	declarations: [
 		PropertyTourRequestShellComponent,
-  PendingTourRequestComponent,
-  TourRequestByPropertyComponent,
-  PendingTourRequestCardComponent
+		PendingTourRequestComponent,
+		TourRequestByPropertyComponent,
+		PendingTourRequestCardComponent
 	],
-  imports: [
-    CommonModule,
+	imports: [
+		CommonModule,
 		SharedModule,
 		RouterModule.forChild(routes)
-  ]
+	]
 })
 export class PropertyTourRequestModule { }
--- src/app/modules/property-tour-request/tour-request-card/tour-request-card.component.spec.ts
+++ src/app/modules/property-tour-request/tour-request-card/tour-request-card.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PendingTourRequestCardComponent } from './pending-tour-request-card.component';
+import { PendingTourRequestCardComponent } from './tour-request-card.component';
 
 describe('PendingTourRequestCardComponent', () => {
   let component: PendingTourRequestCardComponent;

756925a320bc9db01ae4d1e78c19cc9731b4a210
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2024-05-07 23:44:00 +0600

    :sparkles: add: added search in poroperty list page

--- src/app/modules/property-public/property-list/property-list.component.ts
+++ src/app/modules/property-public/property-list/property-list.component.ts
@@ -10,118 +10,124 @@ import { Paginator } from 'primeng/paginator';
 import { PrimeNgPaginatorEventParams } from 'src/app/models/primeNgPaginatorEventParams';
 
 @Component({
-    selector: 'app-property-list',
-    templateUrl: './property-list.component.html',
-    styleUrls: ['./property-list.component.css']
+	selector: 'app-property-list',
+	templateUrl: './property-list.component.html',
+	styleUrls: ['./property-list.component.css']
 })
 
 
 
 export class PropertyListComponent implements OnInit, AfterViewInit {
-    @ViewChild('containerCard') containerCardRef: ElementRef;
+	@ViewChild('containerCard') containerCardRef: ElementRef;
+	@ViewChild('paginator', { static: true }) paginator: Paginator;
 
-    widthOfContainerCard = 0;
-    skeletonCardSize = 270;
-    skeletonCard = new Array<number>(4).fill(0);
-    tabMenuItems: MenuItem[] = [
-        { label: "Buy", icon: '', routerLink: '../buy' },
-        { label: "Rent", icon: '', routerLink: '../rent' },
-    ];
-    activeTabMenu: MenuItem;
-    SellRent = 1;
-    propertyList: PropertyListDto[];
-    city: string = '';
-    searchCity: string = '';
-    sortByParameter: string = 'City';
-    sortDirection: string = 'asc';
+	widthOfContainerCard = 0;
+	skeletonCardSize = 270;
+	skeletonCard = new Array<number>(4).fill(0);
+	tabMenuItems: MenuItem[] = [
+		{ label: "Buy", icon: '', routerLink: '../buy' },
+		{ label: "Rent", icon: '', routerLink: '../rent' },
+	];
+	activeTabMenu: MenuItem;
+	SellRent = 1;
+	propertyList: PropertyListDto[];
+	sortByParameter: string = 'City';
+	sortDirection: string = 'asc';
 
-    sortOptions = [
-        { label: 'City', value: 'City' },
-        { label: 'Price', value: 'Price' }
-    ];
+	sortOptions = [
+		{ label: 'City', value: 'City' },
+		{ label: 'Price', value: 'Price' }
+	];
 
-    rowsPerPageOptions: number[] = [1, 2, 5, 10, 20, 30];
-    totalRecords = 0;
-    rows = 10;
-    currentPage = 0;
+	rowsPerPageOptions: number[] = [1, 2, 5, 10, 20, 30];
+	totalRecords = 0;
+	rows = 10;
+	currentPage = 1;
+	searchingText: string = "";
 
-    isLoading = false;
+	isLoading = false;
 
-    constructor(
-        private route: ActivatedRoute,
-        private propertyService: PropertyService,
-        private elementRef: ElementRef
-    ) { }
+	constructor(
+		private route: ActivatedRoute,
+		private propertyService: PropertyService,
+		private elementRef: ElementRef
+	) { }
 
-    ngOnInit(): void {
-        this.activeTabMenu = this.tabMenuItems[0];
-        let buyOrRent = this.route.snapshot.url.toString();
-        if (buyOrRent === 'buy') {
-            this.SellRent = 1; // Means we are on rent-property URL else we are on base URL
-        } else if (buyOrRent === 'rent') {
-            this.SellRent = 2;
-        }
-        let paginationParamsInitial: PaginationParameter = {
-            currentPageNo: 1,
-            pageSize: this.rows,
-            sortBy: '',
-            isDescending: false,
-            searchField: '',
-            searchingText: ''
-        };
-        this.loadData(paginationParamsInitial);
-    }
+	ngOnInit(): void {
+		this.activeTabMenu = this.tabMenuItems[0];
+		let buyOrRent = this.route.snapshot.url.toString();
+		if (buyOrRent === 'buy') {
+			this.SellRent = 1; // Means we are on rent-property URL else we are on base URL
+		} else if (buyOrRent === 'rent') {
+			this.SellRent = 2;
+		}
+		let paginationParamsInitial: PaginationParameter = {
+			currentPageNo: 1,
+			pageSize: this.rows,
+			sortBy: '',
+			isDescending: false,
+			searchField: '',
+			searchingText: this.searchingText
+		};
+		this.loadData(paginationParamsInitial);
+	}
 
-    ngAfterViewInit(): void {
-        this.widthOfContainerCard = parseInt(this.containerCardRef.nativeElement.clientWidth) ?? 0;
-        this.calculateSkeletonCardNumber();
-    }
+	ngAfterViewInit(): void {
+		this.widthOfContainerCard = parseInt(this.containerCardRef.nativeElement.clientWidth) ?? 0;
+		this.calculateSkeletonCardNumber();
+	}
 
-    @HostListener('window:resize')
-    onResizeHandler() {
-        this.widthOfContainerCard = parseInt(this.elementRef.nativeElement.offsetWidth) ?? 0;
-        this.calculateSkeletonCardNumber();
-    }
+	@HostListener('window:resize')
+	onResizeHandler() {
+		this.widthOfContainerCard = parseInt(this.elementRef.nativeElement.offsetWidth) ?? 0;
+		this.calculateSkeletonCardNumber();
+	}
 
-    onPaginatorPageChange(event:PrimeNgPaginatorEventParams) {
-        this.currentPage = event.page;
-        this.rows = event.rows
-        console.log(event);
-        let paginationParams = Mapper.paginatorEventToPaginationParameter(event);
-        this.loadData(paginationParams);
-    }
+	onPaginatorPageChange(event: PrimeNgPaginatorEventParams) {
+		this.currentPage = event.page;
+		this.rows = event.rows
+		console.log(event);
+		let paginationParams = Mapper.paginatorEventToPaginationParameter(event);
+		paginationParams.searchField = '';
+		paginationParams.searchingText = this.searchingText;
+		this.loadData(paginationParams);
+	}
 
-    loadData(paginationParams: PaginationParameter) {
-        this.isLoading = true;
-        this.propertyService.getPropertyPaginatedList(paginationParams, this.SellRent).subscribe({
-            next: (data) => {
-                this.propertyList = data.resultList;
-                this.totalRecords = data.totalRecords;
-                this.rows = data.pageSize;
-                this.isLoading = false;
-            },
-            error: (err: HttpErrorResponse) => {
-                this.isLoading = false;
-            }
-        });
-    }
+	loadData(paginationParams: PaginationParameter) {
+		this.isLoading = true;
+		this.propertyService.getPropertyPaginatedList(paginationParams, this.SellRent).subscribe({
+			next: (data) => {
+				this.propertyList = data.resultList;
+				this.totalRecords = data.totalRecords;
+				this.currentPage = data.pageNo;
+				this.rows = data.pageSize;
+				this.isLoading = false;
+			},
+			error: (err: HttpErrorResponse) => {
+				this.isLoading = false;
+			}
+		});
+	}
 
-    onCityFilter(clear: boolean = false) {
-        if (clear) {
-            this.city = '';
-        }
-        this.searchCity = this.city;
-    }
 
-    onSortDirection() {
-        this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
-    }
+	onSearch() {
+		var paginationParameter: PaginationParameter = {
+			currentPageNo: 1,
+			pageSize: this.rows,
+			sortBy: '',
+			isDescending: false,
+			searchField: '',
+			searchingText: this.searchingText
+		};
+		this.loadData(paginationParameter);
+		setTimeout(() => this.paginator.changePageToFirst(null));
+	}
 
-    calculateSkeletonCardNumber() {
-        let cardNumber = Math.floor(this.widthOfContainerCard / this.skeletonCardSize);
-        if (cardNumber > 4) {
-            this.skeletonCard = new Array<number>(cardNumber).fill(0);
-        }
+	calculateSkeletonCardNumber() {
+		let cardNumber = Math.floor(this.widthOfContainerCard / this.skeletonCardSize);
+		if (cardNumber > 4) {
+			this.skeletonCard = new Array<number>(cardNumber).fill(0);
+		}
 
-    }
+	}
 }

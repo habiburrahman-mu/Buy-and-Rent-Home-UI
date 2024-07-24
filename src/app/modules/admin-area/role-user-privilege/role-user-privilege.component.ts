7aa6e58dedc4b0baf62fe39853ec55a528a92323
Habibur Rahman <habiburrahman.mu@gmail.com>
2024-07-24 17:40:41 +0600

    updated user list in admin panel

--- src/app/modules/admin-area/role-user-privilege/role-user-privilege.component.ts
+++ src/app/modules/admin-area/role-user-privilege/role-user-privilege.component.ts
@@ -1,7 +1,9 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { LazyLoadEvent, MessageService } from 'primeng/api';
+import { Table } from 'primeng/table';
 import { Subject, takeUntil } from 'rxjs';
+import { PaginationParameter } from 'src/app/models/PaginationParameter';
 import { RoleDto } from 'src/app/models/roleDto';
 import { UserDto } from 'src/app/models/userDto';
 import { UserPrivilege } from 'src/app/models/userPrivilege';
@@ -11,135 +13,144 @@ import { UserService } from 'src/app/services/http/user.service';
 import { Mapper } from 'src/app/utils/mapper';
 
 @Component({
-    selector: 'app-role-user-privilege',
-    templateUrl: './role-user-privilege.component.html',
-    styleUrls: ['./role-user-privilege.component.css']
+	selector: 'app-role-user-privilege',
+	templateUrl: './role-user-privilege.component.html',
+	styleUrls: ['./role-user-privilege.component.css']
 })
 export class RoleUserPrivilegeComponent implements OnInit, OnDestroy {
-    @Input() roleList: RoleDto[] = [];
+	@Input() roleList: RoleDto[] = [];
+	@ViewChild('pTable', { static: true }) pTable: Table;
 
-    private lastTableLazyLoadEvent: LazyLoadEvent;
+	private lastTableLazyLoadEvent: LazyLoadEvent;
 
-    userDtoList: UserDtoExtended[] = [];
-    totalRecords = 0;
+	userDtoList: UserDtoExtended[] = [];
+	totalRecords = 0;
 
-    isDataLoading = false;
+	isDataLoading = false;
 
-    showUserPrivilegeDialog = false;
-    isSaveInProgress = false;
+	showUserPrivilegeDialog = false;
+	isSaveInProgress = false;
 
-    selectedUserPrivilege = {
-        userId: 0,
-        username: "",
-        roleListWithSelectState: [] as Array<RoleDtoWithSelectState>
-    }
+	selectedUserPrivilege = {
+		userId: 0,
+		username: "",
+		roleListWithSelectState: [] as Array<RoleDtoWithSelectState>
+	}
 
-    private ngUnsubscribed = new Subject<void>();
+	searchingText: string = '';
 
-    constructor(
-        private userService: UserService,
-        private userPrivilegeService: UserPrivilegeService,
-        private messageService: MessageService
-    ) { }
+	private ngUnsubscribed = new Subject<void>();
 
-    ngOnInit(): void {
-    }
+	constructor(
+		private userService: UserService,
+		private userPrivilegeService: UserPrivilegeService,
+		private messageService: MessageService
+	) { }
 
-    loadUserDtoList(event: LazyLoadEvent) {
-        this.isDataLoading = true;
-        this.lastTableLazyLoadEvent = event;
-        let paginationParams = Mapper.pTableLazyLoadEventToPaginationParameter(event);
-        this.userService.getUserPaginatedList(paginationParams)
-            .pipe(takeUntil(this.ngUnsubscribed))
-            .subscribe({
-                next: response => {
-                    this.isDataLoading = false;
-                    this.userDtoList = response.resultList;
-                    this.totalRecords = response.totalRecords;
-                    this.userDtoList = this.userDtoList.map(userDto => {
-                        return {
-                            ...userDto,
-                            roleList: userDto.userPrivileges.map(privilege => {
-                                return privilege.roleName;
-                            })
-                        }
-                    });
-                    this.isDataLoading = false;
-                    // console.log(this.userDtoList);
-                },
-                error: error => {
-                    this.isDataLoading = false;
-                    console.log(error);
-                }
-            });
-    }
+	ngOnInit(): void {
+	}
 
-    onShowUserPrivilegeDialog(user: UserDto) {
-        let roleIdListForSelectedUser = user.userPrivileges.map(privilege => {
-            return privilege.roleId;
-        });
-        this.selectedUserPrivilege = {
-            userId: user.id,
-            username: user.username,
-            roleListWithSelectState: this.roleList.map(role => {
-                return {
-                    ...role,
-                    isSelected: roleIdListForSelectedUser
-                        .find(roleId => roleId === role.id) !== undefined
-                }
-            })
-        };
-        this.showUserPrivilegeDialog = true
-    }
+	loadUserDtoList(event: LazyLoadEvent) {
+		this.isDataLoading = true;
+		this.lastTableLazyLoadEvent = event;
+		let paginationParams = Mapper.pTableLazyLoadEventToPaginationParameter(event);
+		paginationParams.searchField = 'username';
+		paginationParams.searchingText = this.searchingText;
+		this.userService.getUserPaginatedList(paginationParams)
+			.pipe(takeUntil(this.ngUnsubscribed))
+			.subscribe({
+				next: response => {
+					this.isDataLoading = false;
+					this.userDtoList = response.resultList;
+					this.totalRecords = response.totalRecords;
+					this.userDtoList = this.userDtoList.map(userDto => {
+						return {
+							...userDto,
+							roleList: userDto.userPrivileges.map(privilege => {
+								return privilege.roleName;
+							})
+						}
+					});
+					this.isDataLoading = false;
+					// console.log(this.userDtoList);
+				},
+				error: error => {
+					this.isDataLoading = false;
+					console.log(error);
+				}
+			});
+	}
 
-    onSubmit() {
-        this.isSaveInProgress = true;
-        let userPrivilegeSaveDto: UserPrivilegeSaveDto = {
-            userId: this.selectedUserPrivilege.userId,
-            userPrivilegeList: this.selectedUserPrivilege.roleListWithSelectState
-                .filter(role => role.isSelected)
-                .map(role => {
-                    let userPrivilege: UserPrivilege = {
-                        id: 0,
-                        userId: this.selectedUserPrivilege.userId,
-                        roleId: role.id
-                    }
-                    return userPrivilege;
-                })
-        }
+	onShowUserPrivilegeDialog(user: UserDto) {
+		let roleIdListForSelectedUser = user.userPrivileges.map(privilege => {
+			return privilege.roleId;
+		});
+		this.selectedUserPrivilege = {
+			userId: user.id,
+			username: user.username,
+			roleListWithSelectState: this.roleList.map(role => {
+				return {
+					...role,
+					isSelected: roleIdListForSelectedUser
+						.find(roleId => roleId === role.id) !== undefined
+				}
+			})
+		};
+		this.showUserPrivilegeDialog = true
+	}
 
-        this.userPrivilegeService.saveUserPrivilege(userPrivilegeSaveDto)
-            .pipe(takeUntil(this.ngUnsubscribed))
-            .subscribe({
-                next: response => {
-                    this.messageService.add({
-                        severity: 'success',
-                        summary: 'User Privilege',
-                        detail: 'User Privilege Updated'
-                    });
-                    this.loadUserDtoList(this.lastTableLazyLoadEvent);
-                    this.isSaveInProgress = false;
-                    this.showUserPrivilegeDialog = false;
-                },
-                error: (error: HttpErrorResponse) => {
-                    console.log(error);
-                    this.isSaveInProgress = false;
-                }
-            });
+	onSubmit() {
+		this.isSaveInProgress = true;
+		let userPrivilegeSaveDto: UserPrivilegeSaveDto = {
+			userId: this.selectedUserPrivilege.userId,
+			userPrivilegeList: this.selectedUserPrivilege.roleListWithSelectState
+				.filter(role => role.isSelected)
+				.map(role => {
+					let userPrivilege: UserPrivilege = {
+						id: 0,
+						userId: this.selectedUserPrivilege.userId,
+						roleId: role.id
+					}
+					return userPrivilege;
+				})
+		}
 
-        console.log(userPrivilegeSaveDto);
-    }
+		this.userPrivilegeService.saveUserPrivilege(userPrivilegeSaveDto)
+			.pipe(takeUntil(this.ngUnsubscribed))
+			.subscribe({
+				next: response => {
+					this.messageService.add({
+						severity: 'success',
+						summary: 'User Privilege',
+						detail: 'User Privilege Updated'
+					});
+					this.loadUserDtoList(this.lastTableLazyLoadEvent);
+					this.isSaveInProgress = false;
+					this.showUserPrivilegeDialog = false;
+				},
+				error: (error: HttpErrorResponse) => {
+					console.log(error);
+					this.isSaveInProgress = false;
+				}
+			});
 
-    ngOnDestroy(): void {
-        this.ngUnsubscribed.next();
-        this.ngUnsubscribed.complete();
-    }
+		console.log(userPrivilegeSaveDto);
+	}
+
+	onSearch() {
+		this.pTable.reset();
+	}
+
+	ngOnDestroy(): void {
+		this.ngUnsubscribed.next();
+		this.ngUnsubscribed.complete();
+	}
 }
 
 interface UserDtoExtended extends UserDto {
-    roleList?: Array<string>
+	roleList?: Array<string>
 }
 
 interface RoleDtoWithSelectState extends RoleDto {
-    isSelected: boolean
+	isSelected: boolean
 }

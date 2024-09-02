69e8ddddeb42196b4ef9ca8f9de3fd20436f5a79
Habibur Rahman <habiburrahman.mu@gmail.com>
2024-09-02 21:52:10 +0600

    property detail resolver updated due to delete functionalities

--- src/app/modules/property-public/property-detail/property-detail-resolver.service.ts
+++ src/app/modules/property-public/property-detail/property-detail-resolver.service.ts
@@ -1,5 +1,6 @@
-import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot} from "@angular/router";
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from "@angular/router";
+import { map } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
 import { of } from 'rxjs/internal/observable/of';
 import { catchError } from 'rxjs/internal/operators/catchError';
@@ -7,19 +8,27 @@ import { PropertyDetailDto } from 'src/app/models/propertyDetailDto';
 import { PropertyService } from 'src/app/services/http/property.service';
 
 @Injectable({
-    providedIn: 'root'
+	providedIn: 'root'
 })
 export class PropertyDetailResolverService implements Resolve<PropertyDetailDto | null> {
 
-    constructor(private router: Router,
-                private propertyService: PropertyService) {
-    }
+	constructor(private router: Router,
+		private propertyService: PropertyService) {
+	}
 
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<PropertyDetailDto | null> | Promise<PropertyDetailDto> | PropertyDetailDto {
-        const propertyId = route.params['id'];
-        return this.propertyService.getPropertyDetail(propertyId).pipe(catchError(error => {
-            this.router.navigate(['/']);
-            return of(null);
-        }));
-    }
+	resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<PropertyDetailDto | null> {
+		const propertyId = route.params['id'];
+		return this.propertyService.getPropertyDetail(propertyId).pipe(
+			map(x => {
+				if (!x) {
+					this.router.navigate(['/']);
+					return null;
+				}
+				return x;
+			}),
+			catchError(error => {
+				this.router.navigate(['/']);
+				return of(null);
+			}));
+	}
 }
